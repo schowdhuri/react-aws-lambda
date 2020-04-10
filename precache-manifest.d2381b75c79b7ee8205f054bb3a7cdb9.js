3d800d65a8c65b04ea2294bb865a389f3694fe54
Subir <schowdhuri@users.noreply.github.com>
2020-04-11 04:28:24 +0530

    Updates

---
+++ precache-manifest.d2381b75c79b7ee8205f054bb3a7cdb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5c2cecd2e02dcc68f186df5ba102023",
+    "url": "/react-aws-lambda/index.html"
+  },
+  {
+    "revision": "d8a8c075c1c8f2382708",
+    "url": "/react-aws-lambda/static/css/main.1e813b89.chunk.css"
+  },
+  {
+    "revision": "bfde13c7b243ea2a4ac0",
+    "url": "/react-aws-lambda/static/js/2.fdae60ba.chunk.js"
+  },
+  {
+    "revision": "835895672b0e80fccc86bf50d23ca6eb",
+    "url": "/react-aws-lambda/static/js/2.fdae60ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8a8c075c1c8f2382708",
+    "url": "/react-aws-lambda/static/js/main.85e26649.chunk.js"
+  },
+  {
+    "revision": "2953cb08b9fc369cee97",
+    "url": "/react-aws-lambda/static/js/runtime-main.25840bc0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.23720c0ee602bf20077c6a7263d10b82.js"
+  "/react-aws-lambda/precache-manifest.d2381b75c79b7ee8205f054bb3a7cdb9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-aws-lambda/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
